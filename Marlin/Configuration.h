600e661c04733c3fdf415a69965aab256fed2d89
rado79 <radorene@gmx.at>
2021-01-28 10:13:37 +0100

    Meatpack enabled

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -986,7 +986,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, 40, -1.60 } //rado
+#define NOZZLE_TO_PROBE_OFFSET { 0, 40, -1.00 } //rado
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
