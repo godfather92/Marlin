339b57a100ec5a05fb143f264f2ffc59fae1527b
rado79 <radorene@gmx.at>
2021-05-01 06:50:42 +0200

    add backlash mm

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -969,7 +969,7 @@
 #if ENABLED(BACKLASH_COMPENSATION)
   // Define values for backlash distance and correction.
   // If BACKLASH_GCODE is enabled these values are the defaults.
-  #define BACKLASH_DISTANCE_MM { 0.0, 0.0, 0 } // (mm)
+  #define BACKLASH_DISTANCE_MM { 0.02, 0.02, 0 } // (mm)
   #define BACKLASH_CORRECTION    1.0       // 0.0 = no correction; 1.0 = full correction
 
   // Add steps for motor direction changes on CORE kinematics
